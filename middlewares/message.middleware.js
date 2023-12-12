d59a2764ac5d9811c308af10e59cfec206322304
0adiy <adityaruva@gmail.com>
2023-12-13 00:02:53 +0530

    fix ddinstagram auto embed

--- middlewares/message.middleware.js
+++ middlewares/message.middleware.js
@@ -5,18 +5,15 @@ import { Client, Message } from "discord.js";
  * @param {Client} client
  */
 function messageMiddleWare(message, client) {
+  const content = message.content;
   const pattern =
-    /(https?:\/\/(www\.)?)instagram(\.com\/(p|reel)\/[A-Za-z0-9_-]+\/?)/g;
-  if (pattern.test(message.content)) {
-    const newContent =
-      message.author +
-      " said : " +
-      message.content.replace(
-        pattern,
-        (m, p1, p2, p3) => p1 + "ddinstagram" + p3
-      );
-    // message.channel.send(newContent); // TODO: fix later
-    // message.delete();
+    /^(https?:\/\/(www\.)?)instagram(\.com\/(p|reel)\/[A-Za-z0-9_-]+\/?)/g;
+  if (pattern.test(content)) {
+    const newContent = content.replace(
+      pattern,
+      (m, p1, p2, p3) => p1 + "ddinstagram" + p3
+    );
+    message.channel.send(newContent);
   }
 }
 
