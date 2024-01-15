7f4e78fcdf812390d6b6e8154be46085c35743ec
0adiy <adityaruva@gmail.com>
2024-01-15 10:56:12 +0530

    add powershell interface

---
+++ commands/powershell.js
@@ -0,0 +1,28 @@
+import { EMOJIS } from "../utils/enums.js";
+import { exec } from "child_process";
+
+export default {
+  name: "powershell",
+  args: ["cmd"],
+  aliases: ["ps"],
+  description: "(only for SuperUsers) Runs a powershell command.",
+  guildOnly: false,
+  /**
+   * Executes the given code in powershell and sends the response back.
+   *
+   * @param {Client} client - The client object.
+   * @param {Message} message - The message object.
+   * @param {Array} args - The arguments passed to the command.
+   */
+  execute: async (client, message, args) => {
+    const code = args.join(" ").replace(/^```\w* |\n?```$/g, "");
+
+    exec(code, { shell: "powershell" }, (error, stdout, stderr) => {
+      if (!(stdout || error || stderr)) return message.react(EMOJIS.CHECK);
+
+      message.reply({
+        content: "```" + `ansi\n${stdout || error || stderr}` + "```",
+      });
+    });
+  },
+};
