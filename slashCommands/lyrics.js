dc1ba1007c92c2c8db3b2e07badf970b814c95c9
ShazamBolt8 <ShazamBolt8@gmail.com>
2024-05-30 03:58:33 +0500

    switched to lyrixcope for better ux

--- slashCommands/lyrics.js
+++ slashCommands/lyrics.js
@@ -1,53 +1,6 @@
 import { SlashCommandBuilder } from "discord.js";
 import config from "../config.js";
-import axios from "axios";
-import { load } from "cheerio";
-
-async function getSongLink(songName) {
-  let responseData = { status: 400, song: null };
-  const api = config.apis.lyrics_api;
-  try {
-    const response = await fetch(
-      `${api.endpoint}?uid=${api.user_id}&tokenid=${
-        api.token
-      }&term=${encodeURIComponent(songName)}&format=${api.format}`
-    );
-    const responseBody = await response.text();
-    if (responseBody == "{}") {
-      responseData.status = 404;
-    } else {
-      let jsonData = JSON.parse(responseBody);
-      responseData.status = 200;
-      responseData.song = jsonData.result[0];
-    }
-  } catch (error) {
-    console.error("Error fetching song link:", error);
-    responseData.status = 500;
-  }
-  return responseData;
-}
-
-async function scrapeLyrics(url) {
-  try {
-    const response = await axios.get(url);
-    const $ = load(response.data);
-    const lyrics = $("pre.lyric-body").text().trim();
-    return lyrics;
-  } catch (error) {
-    console.error("An error occured while scraping song:", error);
-    return null;
-  }
-}
-
-async function getSongLyrics(songName) {
-  const response = await getSongLink(songName);
-  if (response.status == 200) {
-    const lyrics = await scrapeLyrics(response.song["song-link"]);
-    return lyrics;
-  } else {
-    return null;
-  }
-}
+import { configureLyrixcope, getSongLyrics } from "lyrixcope";
 
 export default {
   data: new SlashCommandBuilder()
@@ -59,6 +12,12 @@ export default {
         .setName("song")
         .setDescription("The song to display lyrics for")
         .setRequired(true)
+    )
+    .addStringOption(option =>
+      option
+        .setName("artist")
+        .setDescription("The name of the artist")
+        .setRequired(false)
     ),
   /**
    *
@@ -66,11 +25,17 @@ export default {
    * @param {Client} client
    */
   async execute(interaction, client) {
+    await interaction.deferReply();
+    configureLyrixcope(
+      config.apis.lyrics_api.user_id,
+      config.apis.lyrics_api.token
+    );
     const songName = interaction.options.getString("song");
-    let lyrics = await getSongLyrics(songName);
+    const artistName = interaction.options.getString("artist") ?? "";
+    let response = await getSongLyrics(songName, artistName);
     // TODO - add multi-emebeds (each emebed can have upto 6000 chars while simple message can have upto 2000 chars)
-    // and even maybe pagination
-    let response = lyrics?.slice(0, 1900) ?? "No lyrics found";
-    await interaction.reply(response);
+    // and even maybe paginationr
+    let lyrics = response.status == 200 ? response.lyrics : "No lyrics found";
+    await interaction.followUp(lyrics);
   },
 };
