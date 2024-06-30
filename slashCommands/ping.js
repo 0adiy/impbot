729660693469f4a080ce27ecd5da9c125bac870d
ShazamBolt8 <ShazamBolt8@gmail.com>
2024-07-01 00:42:12 +0500

    improved /ping

--- slashCommands/ping.js
+++ slashCommands/ping.js
@@ -1,9 +1,10 @@
-import { SlashCommandBuilder } from "discord.js";
+import { SlashCommandBuilder, EmbedBuilder} from "discord.js";
+import { COLORS } from "../utils/enums.js";
 
 export default {
   data: new SlashCommandBuilder()
     .setName("ping")
-    .setDescription("Replies with Pong!")
+    .setDescription("Outputs latency of the bot")
     .setDMPermission(false),
   /**
    *
@@ -11,6 +12,12 @@ export default {
    * @param {Client} client
    */
   async execute(interaction, client) {
-    await interaction.reply("Pong!");
+    const embed = new EmbedBuilder()
+      .setTitle(`**${client.ws.ping}ms**`)
+      .setDescription(`The latency of the bot is currently ${client.ws.ping}ms.`)
+      .setThumbnail(client.user.displayAvatarURL({ dynamic: true }))
+      .setTimestamp(new Date())
+      .setColor(COLORS.SUCCESS);
+    await interaction.reply({ embeds: [embed] });
   },
 };
