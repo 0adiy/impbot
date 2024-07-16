d4b019e308f17a0beadb7bbc0d5c9d4b23bc05e3
ShazamBolt8 <ShazamBolt8@gmail.com>
2024-07-17 00:18:34 +0500

    improved /help

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,7 @@
 import { EmbedBuilder } from "discord.js";
 import { COLORS } from "../utils/enums.js";
 import config from "../config.js";
+import { logEvent } from "../utils/generalUtils.js";
 
 function capitalize_First_Letter(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
@@ -22,19 +23,30 @@ export default {
   execute: async (client, message) => {
     let embed = new EmbedBuilder()
       .setTitle(`Available commands`)
-      .setDescription("Additionally you can see slash commands by typing /")
+      .setDescription("DM the bot if you have any complaints.")
       .setColor(COLORS.SECONDARY);
-    client.messageCommands.forEach((cmd) => {
+    client.messageCommands.forEach(cmd => {
+      if (cmd.isPrivate) return;
       const usage = cmd.args
-        ? `${config.prefix}${cmd.name} ${cmd.args.map((a) => `<${a}>`).join(" ")}`
+        ? `${config.prefix}${cmd.name} ${cmd.args.map(a => `<${a}>`).join(" ")}`
         : `${config.prefix}${cmd.name}`;
-
       const description = `${cmd.description}\nAKA ${cmd.aliases.join(", ")}.`;
       embed.addFields({
         name: usage,
         value: description,
       });
     });
-    message.reply({ embeds: [embed] });
+    const slashCommands = await client.application.commands.fetch();
+    slashCommands.forEach(cmd => {
+      embed.addFields({
+        name: `</${cmd.name}:${cmd.id}>`,
+        value: cmd.description,
+      });
+    });
+    try {
+      message.reply({ embeds: [embed] });
+    } catch (e) {
+      await logEvent("ERR", client, e);
+    }
   },
 };
