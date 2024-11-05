71e96c192138253aa729ccdb6c8d991c7ebddb28
ShazamBolt8 <ShazamBolt8@gmail.com>
2024-11-05 20:56:42 +0500

    fixed a single comment

--- events/ready.event.js
+++ events/ready.event.js
@@ -2,25 +2,17 @@ import { Events } from "discord.js";
 import { loadSlashCommands } from "../handlers/slashCommandHandler.js";
 import { loadAndSetAllReminders } from "../utils/generalUtils.js";
 import reminderSchema from "../models/reminder.model.js";
-// import { loadMessageCommands } from "../handlers/messageCommandHandler.js";
-
-const ONE_DAY_IN_MS = 24 * 60 * 60 * 1000;
 
 export default {
   name: Events.ClientReady,
   once: true,
   async execute(client) {
     console.log(`😈 ${client.user.tag} is ready!`);
-
-    // Loading Commands
-    // loadMessageCommands(client);
-
-    // REVIEW - awaits were removed from here, don't think we need to wait for anything actually they can just happen (remove this line after reading)
-    // Call loadAndSetAllReminders immediately when the bot starts and then call it every 24 hours
     loadAndSetAllReminders(reminderSchema, client);
+    // Manually invoked to load reminders scheduled for the near future into memory.
     setInterval(
       () => loadAndSetAllReminders(reminderSchema, client),
-      ONE_DAY_IN_MS
+      24 * 60 * 60 * 1000
     );
 
     loadSlashCommands(client);
