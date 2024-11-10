016e054b2a66f8b55716bdefa62e7dfbe05b1a56
ShazamBolt8 <ShazamBolt8@gmail.com>
2024-11-10 22:12:29 +0500

    kinda fixed contextual ai

--- commands/gemini.js
+++ commands/gemini.js
@@ -1,5 +1,6 @@
 import { EmbedBuilder } from "discord.js";
 import { logEvent } from "../utils/generalUtils.js";
+import config from "../config.js";
 
 export default {
   name: "gemini",
@@ -15,9 +16,8 @@ export default {
    * @param {Message} message
    */
   execute: async (client, message) => {
-    const model = client.contextualAI;
-
     const MIN_PROMPT_LENGTH = 1500;
+    const MSG_FETCH_LIMIT = 5;
     let prompt = "";
     let fetchedMessages = [];
     let lastMessageId = null;
@@ -28,28 +28,36 @@ export default {
 
     while (prompt.length < MIN_PROMPT_LENGTH) {
       let messages = await message.channel.messages.fetch({
-        limit: 5,
+        limit: MSG_FETCH_LIMIT,
         ...(lastMessageId ? { before: lastMessageId } : {}),
       });
+
       if (messages.size == 0) break;
+
+      fetchedMessages.push(...Array.from(messages.values()));
+
+      lastMessageId = messages.last().id;
+
       messages = messages.filter(
-        message =>
-          !message.author.bot ||
-          message.author.id === client.user.id ||
-          !["$g", "$ai", "$gemini"].includes(message.content)
+        msg =>
+          (msg.author.id === client.user.id || !msg.author.bot) &&
+          !msg.content.startsWith(config.prefix)
       );
-      messages.sort((a, b) => a.createdTimestamp - b.createdTimestamp);
-      messages.forEach(message => {
-        prompt += `${message.author.username.slice(0, 3)}: ${
-          message.content
-        }\n`;
-        lastMessageId = message.id;
+
+      messages.forEach(msg => {
+        prompt += `${msg.author.username.slice(0, 3)}: ${msg.content}\n`;
       });
+
       if (prompt.length >= MIN_PROMPT_LENGTH) break;
+
+      fetchedMessages.sort((a, b) => a.createdTimestamp - b.createdTimestamp);
+      prompt = "";
+      fetchedMessages.forEach(msg => {
+        prompt += `${msg.author.username.slice(0, 3)}: ${msg.content}\n`;
+      });
     }
     try {
-      logEvent("CTX", client, prompt);
-      let result = await model.generateContent(prompt);
+      let result = await client.contextualAI.generateContent(prompt);
       const response = result.response.text();
       message.channel.send(response);
     } catch (error) {
