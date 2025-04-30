fe36a02f8e2458e89c9b487872d297b405afcb14
ShazamBolt8 <shazambolt8@gmail.com>
2025-05-01 00:42:36 +0500

    bug fixes

--- events/messageReactionAdd.event.js
+++ events/messageReactionAdd.event.js
@@ -7,13 +7,14 @@ export default {
    *
    * @param {MessageReaction} reaction - The reaction object.
    * @param {User} user - The user object.
+   * @param {Boolean} superReaction - The super reaction object.
    * @param {Client} client - The client object.
    */
-  async execute(reaction, user, client) {
+  async execute(reaction, user, superReaction, client) {
     if (user.bot) return;
     const emoji = reaction.emoji.name.toLowerCase();
     console.log(client);
-    
+
     const command = client.reactionCommands.find(cmd =>
       cmd.reactions?.includes(emoji)
     );
