6fa89991410781798d539e9a8e2cfc4a62cdcdeb
ShazamBolt8 <shazambolt8@gmail.com>
2025-05-12 23:13:02 +0500

    /core restarts the bot

--- components/modals/config.modal.js
+++ components/modals/config.modal.js
@@ -5,6 +5,9 @@ import {
   ActionRowBuilder,
 } from "discord.js";
 import fs from "fs/promises";
+import { exec } from "child_process";
+import util from "util";
+const execProm = util.promisify(exec);
 
 const fileContents = await fs.readFile("./config.js", { encoding: "utf8" });
 const customId = "configModal";
@@ -26,11 +29,15 @@ export default {
   isPrivate: true,
   async execute(interaction, client) {
     const newContent = interaction.fields.getTextInputValue("content");
-    // const oldContent = await fs.readFile("./config.js", { encoding: "utf8" });
-    // await fs.writeFile("./config.backup.js", oldContent);
+    const oldContent = await fs.readFile("./config.js", { encoding: "utf8" });
+    await fs.writeFile("./config.backup.js", oldContent);
     await fs.writeFile("./config.js", newContent);
     await interaction.reply({
-      content: "Successfully updated `config.js`.",
+      content:
+        "Successfully updated `config.js`.\nOld content has been saved to `config.backup.js`.\nRestarting the bot...",
     });
+    setTimeout(async () => {
+      await execProm("pm2 restart impbot");
+    }, 2000);
   },
 };
