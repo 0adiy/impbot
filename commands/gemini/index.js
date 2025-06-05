b7cb3ba47a014131cf95a00119e7568b1ec75a2f
ShazamBolt8 <shazambolt8@gmail.com>
2025-06-05 17:49:57 +0500

    refactored gemini

---
+++ commands/gemini/index.js
@@ -0,0 +1,42 @@
+import { CommandType } from "../../constants/commandTypes.js";
+import { CommandCategory } from "../../constants/commandCategories.js";
+import { CommandPrivacy } from "../../constants/commandPrivacy.js";
+import { CommandScope } from "../../constants/commandScope.js";
+import { logEvent } from "../../utils/generalUtils.js";
+import * as util from "./util.js";
+
+export default {
+  name: "gemini",
+  privacy: CommandPrivacy.PUBLIC,
+  category: CommandCategory.FUN,
+  scope: CommandScope.GUILD,
+  type: CommandType.MESSAGE,
+  args: [],
+  aliases: ["g", "ai"],
+  description: "Summon Gemini to join the conversation",
+  /**
+   * Summons AI and makes it participate in the conversation
+   *
+   * @param {Client} client
+   * @param {Message} message
+   */
+  execute: async (client, message) => {
+    try {
+      await message.channel.sendTyping();
+      const history = await util.getChatHistory(message.channel);
+      let payload = history;
+      if (message.content.split(" ").length > 1) {
+        payload = `Request:\n${message.content.substr(
+          message.content.indexOf(" ") + 1
+        )}\n\nChat context:\n${history}`;
+      }
+      const result = await client.contextualAI.generateContent(payload);
+      const response = result.response.text();
+      // don't log context anymore
+      // await logEvent("CTX", client, payload);
+      message.channel.send(response);
+    } catch (error) {
+      await logEvent("ERR", client, error);
+    }
+  },
+};
---
+++ commands/gemini/util.js
@@ -0,0 +1,31 @@
+  /**
+   * Retrieves the last 1500 characters of chat history from the specified channel
+   * @param {TextChannel} channel - The channel to retrieve the history from
+   * @returns {string} The retrieved chat history
+   */
+export async function getChatHistory(channel) {
+  let history = "";
+  let lastMessageId = null;
+  const messagesList = [];
+
+  while (history.length < 1500) {
+    const messages = await channel.messages.fetch({
+      limit: 10,
+      before: lastMessageId,
+    });
+    if (messages.size === 0) break;
+
+    messages.forEach(msg => messagesList.push(msg));
+    lastMessageId = messages.last().id;
+
+    const temp = messagesList.map(m => m.content).join("\n");
+    if (temp.length >= 1500) break;
+  }
+
+  messagesList.sort((a, b) => a.createdTimestamp - b.createdTimestamp);
+
+  return messagesList
+    .filter(msg => !msg.content.startsWith(config.prefix))
+    .map(msg => `${msg.author.username.slice(0, 3)}: ${msg.content}`)
+    .join("\n");
+}
