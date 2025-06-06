99585736b6a442c42f43d37fbfafaee172b3c337
ShazamBolt8 <shazambolt8@gmail.com>
2025-06-06 12:20:59 +0500

    added core  to commands

---
+++ commands/core/core.js
@@ -0,0 +1,38 @@
+import { CommandType } from "../../constants/commandTypes.js";
+import { CommandCategory } from "../../constants/commandCategories.js";
+import { CommandPrivacy } from "../../constants/commandPrivacy.js";
+import {
+  SlashCommandBuilder,
+  InteractionContextType,
+  ApplicationIntegrationType,
+} from "discord.js";
+import configModal from "../../components/modals/config.modal.js";
+
+const core = {
+  name: "core",
+  type: CommandType.SLASH,
+  category: CommandCategory.UTILITY,
+  privacy: CommandPrivacy.PUBLIC,
+};
+
+export default {
+  ...core,
+  data: new SlashCommandBuilder()
+    .setName("core")
+    .setDescription("Edit config.js file of the bot")
+    .setIntegrationTypes([ApplicationIntegrationType.GuildInstall])
+    .setContexts([
+      InteractionContextType.Guild,
+      InteractionContextType.PrivateChannel,
+    ]),
+  isPrivate: true,
+  /**
+   *
+   * @param {ChatInputCommandInteraction} interaction
+   * @param {Client} client
+   */
+  async execute(interaction, client) {
+    const modal = configModal.data;
+    await interaction.showModal(modal);
+  },
+};
